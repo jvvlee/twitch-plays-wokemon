c345688d48411a8bbf1d18ac64f49cff01d33157
Jonathan Lee <jlee@movableink.com>
2018-02-15 19:51:27 -0500

    First draft of twitch reader.

--- twitch-reader.go
+++ twitch-reader.go
@@ -6,6 +6,39 @@ import(
   "fmt"
 )
 
-var config = twitchchat.NewConfiguration(
-
+var (
+  config *twitchchat.Configuration
 )
+
+func disconnect() {
+  fmt.Println("Disconnected!")
+}
+
+func connect() {
+  fmt.Println("Connected!")
+}
+
+func newMessage(message string) {
+  fmt.Println(message)
+}
+
+func main() {
+  config := twitchchat.NewConfiguration(
+    "test",
+    "oauth:ex9ki2l39r579fkzlk7ry4h3zdb4hy",
+    "test",
+  )
+
+  twitch := twitchchat.NewChat(config)
+
+  stop := make(chan struct{})
+  defer close(stop)
+
+  err := twitch.ConnectWithCallbacks(connect, disconnect, newMessage)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  <- stop
+}
