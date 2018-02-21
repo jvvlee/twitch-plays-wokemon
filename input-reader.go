70a8f1e43f6a003760356ddf39d7222e5e36b38d
Jonathan Lee <jlee@movableink.com>
2018-02-20 22:30:55 -0500

    Discovered the power of fmt.

--- input-reader.go
+++ input-reader.go
@@ -1,62 +1,62 @@
 package main
 
 import (
-    "log"
-    "os"
-    "bufio"
-    "github.com/go-vgo/robotgo"
-    "errors"
+	"bufio"
+	"errors"
+	"github.com/go-vgo/robotgo"
+	"log"
+	"os"
 )
 
 type Command string
 
 type CommandInterpreter interface {
-  parseCommand(string) (Command, error)
+	parseCommand(string) (Command, error)
 }
 
-type CommandInterpretron struct {}
+type CommandInterpretron struct{}
 
 func (c *CommandInterpretron) parseCommand(chat string) (Command, error) {
-  value, allowed := allowed_keys[chat] 
+	value, allowed := allowed_keys[chat]
 
-  if allowed {
-    return value, nil
-  } else {
-    return "", errors.New("Command not recognized.")
-  }
+	if allowed {
+		return value, nil
+	} else {
+		return "", errors.New("Command not recognized.")
+	}
 }
 
 var allowed_keys = map[string]Command{
-    "Left": "Left",
-    "Right": "Right",
-    "Up": "Up",
-    "Down": "Down",
-    "A": "Z",
-    "B": "X",
-    "Start": "Enter",
+	"Left":  "Left",
+	"Right": "Right",
+	"Up":    "Up",
+	"Down":  "Down",
+	"A":     "Z",
+	"B":     "X",
+	"Start": "Enter",
 }
 
 func main() {
-  file, err := os.Open("file.txt")
-  if err != nil {
-    log.Fatal(err)
-  }
+	file, err := os.Open("file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  commandinterpreter := new(CommandInterpretron)
+	commandinterpreter := new(CommandInterpretron)
 
-  defer file.Close()
+	defer file.Close()
 
-  scanner := bufio.NewScanner(file)
-  
-  for scanner.Scan() {
-    command := scanner.Text()
+	scanner := bufio.NewScanner(file)
 
-    if interpretedCommand, err := commandinterpreter.parseCommand(command); err == nil {
-      robotgo.TypeString(string(interpretedCommand));
-    }
-  }
+	for scanner.Scan() {
+		command := scanner.Text()
 
-  if err := scanner.Err(); err != nil {
-    log.Fatal(err)
-  }
+		if interpretedCommand, err := commandinterpreter.parseCommand(command); err == nil {
+			robotgo.TypeString(string(interpretedCommand))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
--- twitch-reader.go
+++ twitch-reader.go
@@ -1,42 +1,42 @@
 package main
 
-import(
-  log "github.com/Sirupsen/logrus"
-  twitchchat "github.com/dimorinny/twitch-chat-api"
-  "fmt"
-  "os"
+import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	twitchchat "github.com/dimorinny/twitch-chat-api"
+	"os"
 )
 
 var config *twitchchat.Configuration = twitchchat.NewConfiguration(
-    "testicles",
-    os.Getenv("TWITCH_OAUTH"),
-    "data_dave",
+	"testicles",
+	os.Getenv("TWITCH_OAUTH"),
+	"data_dave",
 )
 
 func disconnect() {
-  fmt.Println("Disconnected!")
+	fmt.Println("Disconnected!")
 }
 
 func connect() {
-  fmt.Println("Connected!")
+	fmt.Println("Connected!")
 }
 
 func newMessage(message string) {
-  fmt.Println(message)
+	fmt.Println(message)
 }
 
 func main() {
 
-  twitch := twitchchat.NewChat(config)
+	twitch := twitchchat.NewChat(config)
 
-  stop := make(chan struct{})
-  defer close(stop)
+	stop := make(chan struct{})
+	defer close(stop)
 
-  err := twitch.ConnectWithCallbacks(connect, disconnect, newMessage)
+	err := twitch.ConnectWithCallbacks(connect, disconnect, newMessage)
 
-  if err != nil {
-    log.Fatal(err)
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  <- stop
+	<-stop
 }
